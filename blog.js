fa4ec73fe754b892f9de2b82cf9321400c9bbf3d
Kevin Yien <kevinyien@Yiens-iMac.local>
2022-06-02 20:56:06 -0500

    publishing creative interfaces post

--- blog.js
+++ blog.js
@@ -1,4 +1,5 @@
 const blogList = {
+  "Creative Interfaces": "./blog/creative-interfaces.html",
   "Full Stack Design": "./blog/full-stack-design.html",
   "Joining the Mutiny": "./blog/mutiny.html",
   "The Unsell Email": "./blog/unsell.html",
